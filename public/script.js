52dd016fbc29a0e2a34ecb47b55592e2b69be796
stevie35 <stevie.leveque@outlook.com>
2020-12-13 16:22:00 +0100

    starting installation new user

--- public/script.js
+++ public/script.js
@@ -15,7 +15,15 @@ navigator.mediaDevices.getUserMedia({
     addVideoStream(myVideo, stream);
 })
 
-socket.emit('join-room');
+socket.emit('join-room', ROOM_ID);
+
+socket.on('user-connected', () => {
+    connecToNewUser();
+})
+
+const connecToNewUser = () => {
+    console.log('new user');
+}
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
